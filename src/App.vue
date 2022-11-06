db12424a041bd1defbfbb78608b75dd2319771d3
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-11-06 13:24:19 +0400

    Add font-awesome package

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="text-3xl font-bold underline">
+    <font-awesome-icon icon="fa-solid fa-user-secret" />
     Vue3 weather app
   </h1>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+// the fontawesome core, icon component
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+// add icons
+library.add(faUserSecret)
+
 import './assets/tailwind.css';
 
 createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
