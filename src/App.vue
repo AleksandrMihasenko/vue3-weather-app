ac0dbff4ae39a453e864857bcc67b9ef5a511db5
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-12-21 18:59:35 +0400

    Implement dynamic page title and transition

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <Navigation />
 
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -10,3 +14,13 @@
 import { Navigation } from '@/components/Navigation';
 
 </script>
+
+<style lang="sass">
+  .page-enter-active,
+  .page-leave-active
+    transition: .5s ease all
+
+  .page-enter-from,
+  .page-leave-to
+    opacity: 0
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,38 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '@/views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 import CityView from '@/views/CityView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'HomeView',
-    component: Home,
+    component: HomeView,
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: '/weather/:state/:city',
     name: 'CityView',
-    component: CityView
+    component: CityView,
+    meta: {
+      title: 'Weather'
+    }
   }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state
+      ? `${to.params.city}, ${to.params.state}`
+      : to.meta.title
+  } | The local weather`;
+  next();
 });
 
 export default router;
